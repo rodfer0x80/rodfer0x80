29727627886ae6e9424a79480fb0a6cd38842b97
rodfer0x80 <rodrigolf080@gmail.com>
2022-10-28 01:59:13 +0100

    .

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,24 @@
-cd docs && python3 -m http.server 8080
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+        set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+        echo 'Usage: ./' + '$0' + 'arg-one arg-two'
+        exit
+fi
+
+CDIR=$(dirname $0)
+WORKDIR="$CDIR/../docs"
+cd $WORKDIR
+
+main() {
+    local debug_port=8080
+    python3 -m http.server $debug_port
+}
+
+main "$@"
+
